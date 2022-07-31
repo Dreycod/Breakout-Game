4cb3d43d6a5b38383a0349f06cb1b8a8943c7891
Dreycod <84332780+Dreycod@users.noreply.github.com>
2022-07-31 08:28:26 +0200

    Player Movement + Physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
-    [SerializedField] 
-    RigidBody2D myRigidBody;
+    [SerializeField] float BarSpeed = 10f;
+
+    Rigidbody2D myRigidBody;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        BarMovement();
     }
 
     private void BarMovement()
     {
         float controlthrow = CrossPlatformInputManager.GetAxis("Horizontal");
 
-        Vector2 playerVelocity = new Vector2(controlthrow * myRigidBody.Velocity.x, 0);
+        Vector2 playerVelocity = new Vector2(controlthrow * BarSpeed, 0);
+        myRigidBody.velocity = playerVelocity;
     }
 }
